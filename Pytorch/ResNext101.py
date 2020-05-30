823a7423d03d0b986d1013fc1bc9e521ae611062
jojimonv <jojimon.varghese@intel.com>
2020-05-30 09:39:47 -0700

    Fixes

--- Pytorch/ResNext101.py
+++ Pytorch/ResNext101.py
@@ -2,33 +2,36 @@
 import torch
 import torch.nn as nn
 
+__all__ = ['resnext50_32x4d', 'resnext101_32x8d']
 
-def conv3x3(in_planes, out_planes, stride=1, groups=1, dilation=1):
+model_urls = {
+  'resnext50_32x4d' : 'https://download.pytorch.org/models/resnext50_32x4d=7cdf4587.pth',
+  'resnext101_32x8d' : 'https://download.pytorch.org/models/resnext101_32x8d=8ba56ff5.pth',
+}
+
+
+def conv3x3(in_planes, out_planes, stride=1, groups=1):
   return nn.Conv2d(in_planes, out_planes, kernel_sze=3, stride=stride,
-                   padding=dilation, groups=groups, bias=False, dilation=dilation)
+                   groups=groups, bias=False)
 
 def conv1x1(in_planes, out_planes, stride=1):
-  return nn.Conv2d(in_planes, out_planes, stride=1):
+  return nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=1)
 
 
 class BasicBlock(nn.Module):
   expansion =1
   
   def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
-               base_width=64, dilation=1, norm_layer=None):
+               base_width=64) :
     super(BasicBlock, self).__init__()
-    if norm_layer is None:
-      norm_layer=nn.BatchNorm2d
     if groups !=1 or base_width !=64:
       raise ValueError('BasicBlock only supports groups=1, and base_width=64')
-    if dilation >1:
-      raise NotImplementedError("Dilation >1 is not supported in BasicBlock")
     
     self.comv1 = conv3x3(inplanes, planes, stride)
-    self.bn1   = norm_layer(planes)
+    self.bn1   = nn.BatchNorm2d(planes)
     self.relu  = nn.ReLU(inplace=True)
     self.conv2 = conv3x3(planes, planes)
-    self.bn2   = norm_layer(planes)
+    self.bn2   = nn.BatchNorm2d(planes)
     self.downsample = downsample
     self.stride = stride
 
@@ -41,7 +44,7 @@ class BasicBlock(nn.Module):
     out = self.conv2(out)
     out = self.bn2(out)
 
-    if self.downsample in not None:
+    if self.downsample is not None:
        identify = self.downsample(x)
 
     out += identify
@@ -54,18 +57,15 @@ class BottleNeck(nn.Module):
   
   expansion = 4
   
-  def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1,
-               base_width=64, dilation=1, norm_layer=None)
+  def __init__(self, inplanes, planes, stride=1, downsample=None, groups=1, base_width=64):
     super(BottleNeck, self).__init__()
-    if norm_layer is None:
-      norm_layer = nn.BatchNorm2d
-    width = int(planes * base_width/64.)) * groups
+    width = int(planes * base_width/64.) * groups
     self.conv1 = conv1x1(inplanes, width)
-    self.bn1   = norm_layer(width)
-    self.conv2 = conv3x3(width, width, stride, groups, dilation)
-    self.bn2   = norm_layer(width)
+    self.bn1   = nn.BatchNorm2d(width)
+    self.conv2 = conv3x3(width, width, stride, groups)
+    self.bn2   = nn.BatchNorm2d(width)
     self.conv3 = conv1x1(width, planes * self.expansion)
-    self.bn3   = norm_layer(planes * self.expansion)
+    self.bn3   = nn.BatchNorm2d(planes * self.expansion)
     self.relu  = nn.ReLU(inplace=True)
     self.downsample = downsample
     self.stride = stride
@@ -94,33 +94,19 @@ class BottleNeck(nn.Module):
 
 class ResNet(nn.Module):
   
-  def __init(self, block, layers, num_classes=1000, zero_init_residual=False,
-             groups=1, width_per_group=64, replace_stride_with_dilation=None,
-             norm_layer=None):
+  def __init(self, block, layers, num_classes=1000, groups=1, width_per_group=64) :
     super(ResNet, self).__init__()
-    if norm_layer in None:
-      norm_layer = nn.BatchNoem2d
-    self._norm_layer= norm_layer
 
     self.inplanes = 64
-    self.dilation = 1
 
-    if replace_stride_with_dilation is None:
-      replace_strid_with_dilation= [False, False, False]
-    if len(replace_stride_with_dilation) !=3:
-      raise ValueError("replace_stride_with_dilation shoule be None"
-                       "or a 3-element tuple got {}".format(replace_stride_with_dilation))
-
-    self.groups = groups
-    self.base_width = width_per_group
-    self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3, bias=False
-    self.bn1 = norm_layer(self.inplanes)
+    self.conv1 = nn.Conv2d(3, self.inplanes, kernel_size=7, stride=2, padding=3, bias=False)
+    self.bn1 = nn.BatchNorm2d(self.inplanes)
     self.relu = nn.ReLU(inplace=True)
     self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
     self.layer1 = self._make_layer(block, 64,  layers[0])
-    self.layer2 = self._make_layer(block, 128, layers[1], stride=2, dilate = replace_stride_with_dilation[0]) 
-    self.layer2 = self._make_layer(block, 256, layers[2], stride=2, dilate = replace_stride_with_dilation[1]) 
-    self.layer2 = self._make_layer(block, 512, layers[3], stride=2, dilate = replace_stride_with_dilation[2]) 
+    self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+    self.layer2 = self._make_layer(block, 256, layers[2], stride=2)
+    self.layer2 = self._make_layer(block, 512, layers[3], stride=2)
     
     self.avgpool = nn.AdaptiveAvgPool2d((1,1))
     self.fc = nn.Linear(512 * block.expansion, num_classes)
@@ -132,34 +118,21 @@ class ResNet(nn.Module):
         nn.init.constant_(m.weight, 1)
         nn.init.constant_(m.bias, 0)
 
-    if zero_init_residual:
-      for m in self.modules():
-        if isinstance(m, Bottleneck):
-          nn.init.constant_(m.bn3.weight,0)
-        elif isinstance(m, BasicBlock):
-          nn.init.constant(m.bn2.weight, 0)
-
  
-  def _make_layer(self, block, planes. blocks, stride=1, dilate=False):
-    norm_layer = self._norm_layer
+  def _make_layer(self, block, planes, blocks, stride=1):
     downsample = None
-    previous_dilation = self.dilation
-    if dilate :
-      self.dilation *=stride
-      stride = 1
     if stride !=1 or self.inplanes != planes * block.expansion:
       downsample = nn.Sequential(
            conv1x1(self.inplanes, planes * block.expansion, stride),
-           norm_layer(planes * block.expansion))
+           nn.BatchNorm2d(planes * block.expansion))
      
     layers = []
     layers.append(block(self.inplanes, planes, stride, downsample, self.groups,
-                        self.base_width, previous_dilation, norm_layer))
+                        self.base_width))
     self.inplanes = planes * block.expansion
     for _ in range(1, blocks):
       layers.append(block(self.inplanes, planes, groups=self.groups,
-                          base_width=self.base_width, dilation=self.dilation,
-                          norm_layer=norm_layer))
+                          base_width=self.base_width))
 
     return nn.Sequential(*layers)
 
